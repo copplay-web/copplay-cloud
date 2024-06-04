d05278d2395d0f11edc8e2dd4181b3e277012ac3
CopPlay <master@copplay.com>
2024-06-05 01:14:30 +0800

    删除模拟token模块

--- copplay-modules/snowy-plugin-sys/snowy-plugin-sys-func/src/main/java/vip/xiaonuo/sys/web/SysRoleController.java
+++ copplay-modules/snowy-plugin-sys/snowy-plugin-sys-func/src/main/java/vip/xiaonuo/sys/web/SysRoleController.java
@@ -1,15 +1,3 @@
-/*
- * Copyright [2022] [https://www.xiaonuo.vip]
- *
- * Snowy采用APACHE LICENSE 2.0开源协议，您在使用过程中，需要注意以下几点：
- *
- * 1.请不要删除和修改根目录下的LICENSE文件。
- * 2.请不要删除和修改Snowy源码头部的版权声明。
- * 3.本项目代码可免费商业使用，商业使用请保留源码和相关描述文件的项目出处，作者声明等。
- * 4.分发源码时候，请注明软件出处 https://www.xiaonuo.vip
- * 5.不可二次分发开源参与同类竞品，如有想法可联系团队xiaonuobase@qq.com商议合作。
- * 6.若您的项目无法满足以上几点，需要更多功能代码，获取Snowy商业授权许可，请在官网购买授权，地址为 https://www.xiaonuo.vip
- */
 package vip.xiaonuo.sys.web;
 
 import cn.hutool.core.lang.tree.Tree;
@@ -27,10 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.copplay.common.annotation.CommonLog;
 import com.copplay.common.domain.CommonResult;
 import vip.xiaonuo.sys.domain.SysRole;
-import vip.xiaonuo.sys.modular.role.param.*;
-import vip.xiaonuo.sys.modular.role.result.*;
-import vip.xiaonuo.sys.param.*;
-import vip.xiaonuo.sys.result.*;
 import vip.xiaonuo.sys.service.SysRoleService;
 import vip.xiaonuo.sys.domain.SysUser;
 import vip.xiaonuo.sys.web.param.*;
